a05ab2d84ced50faa5c6e92a839c9f3414c9d82e
Pandey-Noah-3816 <49300644+Pandey-Noah-3816@users.noreply.github.com>
2021-01-30 22:02:22 -0800

    Add files via upload

---
+++ leapyear_with_check.py
@@ -0,0 +1,40 @@
+"""
+To run this program run python3 noah_pandey_hw1.py
+
+enter the year when prompted
+
+note: this program only runs once (no while loop) so if you want
+to test multiple years you will have to run it multiple times.
+
+
+"""
+
+#set initial condition and get input
+leapyear = False
+try:
+  userin = int(input("Enter a year: "))
+
+except ValueError:
+  print("Input is not a number or is negative! ")
+  quit()
+  
+
+
+#check conditions for leapyear = True or leapyear = False
+
+if userin%4 == 0:
+  leapyear = True
+
+  if userin%100 == 0:
+    leapyear = False
+
+    if userin%400 == 0:
+      leapyear = True
+
+#print out results
+
+if leapyear == True:
+  print (userin, "is a leap year.")
+
+else:
+  print (userin, "is not a leap year.")
